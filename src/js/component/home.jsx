498dde2392f4c0dcfec45b8b63ac7a2617229442
cpizarroi <c.pizarroibaceta@gmail.com>
2024-09-23 22:55:30 -0300

    To do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,124 @@
 import React from "react";
+import { useEffect, useRef, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Todolist() {
+  let nombreRef = useRef(null);
+  const [task, setTask] = useState([]);
+  const [urlApi] = useState("http://assets.breatheco.de/apis/fake/todos/user/cpizarroi");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  useEffect(() => {
+    getTask(urlApi);
+  }, []);
 
-export default Home;
+  const getTask = (url) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setTask(data)) // Guardar tareas en el estado
+      .catch((error) => console.log(error));
+  };
+
+  const getUser = (url) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data.result))
+      .catch((error) => console.log(error));
+  };
+
+  const updateTask = (url, task) => {
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(task),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
+
+  const addTask = (e) => {
+    if (e.keyCode === 13 && nombreRef.current.value !== "") {
+      const newTask = { label: nombreRef.current.value, done: false };
+      const updatedTasks = task.concat(newTask);
+      setTask(updatedTasks);
+      updateTask(urlApi, updatedTasks); // Actualiza las tareas en la API
+      nombreRef.current.value = "";
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = task.filter((_, i) => i !== index);
+    setTask(updatedTasks);
+    updateTask(urlApi, updatedTasks); // Actualiza las tareas en la API
+  };
+
+  const deleteAll = () => {
+    fetch(urlApi, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="container">
+      <div className="card mt-4">
+        <div className="card-body">
+          <h1 className="card-title text-center">
+            To do list <i className="fas fa-tasks"></i>
+          </h1>
+          <ul className="list-group list-group-flush">
+            <div className="input-group mb-2 list-group list-group-flush">
+              <input
+                onKeyUp={addTask}
+                ref={nombreRef}
+                type="text"
+                id="input"
+                className="list-group-item"
+                placeholder="What needs to be done?"
+              />
+            </div>
+
+            {task.length === 0 && (
+              <li className="list-group-item text-center">
+                No tasks, add tasks
+              </li>
+            )}
+
+            {task.length > 0 &&
+              task.map((valor, index) => {
+                return (
+                  <li
+                    className="list-group-item d-flex justify-content-between align-items-center"
+                    key={index}
+                  >
+                    {valor.label}
+                    <i
+                      className="fas fa-trash delete-icon"
+                      onClick={() => deleteTask(index)}
+                    ></i>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div className="card-footer text-muted">
+          Things to do {task.length}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todolist;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,22 @@
-//import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import Todolist from "./component/home.jsx";
 
 //import your own components
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.render(<Home />, document.querySelector("#app"));
 
+ReactDOM.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+  reportWebVitals();
