9d3c115a4edf556f45899c294da17de0bcf6d8be
cpizarroi <c.pizarroibaceta@gmail.com>
2024-09-23 23:20:03 -0300

    To do list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,74 +1,19 @@
-import React from "react";
-import { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 function Todolist() {
   let nombreRef = useRef(null);
   const [task, setTask] = useState([]);
-  const [urlApi] = useState("http://assets.breatheco.de/apis/fake/todos/user/cpizarroi");
-
-  useEffect(() => {
-    getTask(urlApi);
-  }, []);
-
-  const getTask = (url) => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setTask(data)) // Guardar tareas en el estado
-      .catch((error) => console.log(error));
-  };
-
-  const getUser = (url) => {
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify([]),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data.result))
-      .catch((error) => console.log(error));
-  };
-
-  const updateTask = (url, task) => {
-    fetch(url, {
-      method: "PUT",
-      body: JSON.stringify(task),
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  };
 
   const addTask = (e) => {
-    if (e.keyCode === 13 && nombreRef.current.value !== "") {
-      const newTask = { label: nombreRef.current.value, done: false };
-      const updatedTasks = task.concat(newTask);
-      setTask(updatedTasks);
-      updateTask(urlApi, updatedTasks); // Actualiza las tareas en la API
-      nombreRef.current.value = "";
+    if (e.key === "Enter" && nombreRef.current.value.trim() !== "") {
+      setTask([...task, nombreRef.current.value]); 
+      nombreRef.current.value = ""; 
     }
   };
 
   const deleteTask = (index) => {
     const updatedTasks = task.filter((_, i) => i !== index);
-    setTask(updatedTasks);
-    updateTask(urlApi, updatedTasks); // Actualiza las tareas en la API
-  };
-
-  const deleteAll = () => {
-    fetch(urlApi, {
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
+    setTask(updatedTasks); 
   };
 
   return (
@@ -78,43 +23,39 @@ function Todolist() {
           <h1 className="card-title text-center">
             To do list <i className="fas fa-tasks"></i>
           </h1>
+          <div className="input-group mb-2">
+            <input
+              onKeyUp={addTask}
+              ref={nombreRef}
+              type="text"
+              className="list-group-item"
+			  style={{ width: '100%' }}
+
+              placeholder="What needs to be done?"
+            />
+          </div>
           <ul className="list-group list-group-flush">
-            <div className="input-group mb-2 list-group list-group-flush">
-              <input
-                onKeyUp={addTask}
-                ref={nombreRef}
-                type="text"
-                id="input"
-                className="list-group-item"
-                placeholder="What needs to be done?"
-              />
-            </div>
-
-            {task.length === 0 && (
-              <li className="list-group-item text-center">
-                No tasks, add tasks
-              </li>
-            )}
-
-            {task.length > 0 &&
-              task.map((valor, index) => {
-                return (
-                  <li
-                    className="list-group-item d-flex justify-content-between align-items-center"
-                    key={index}
-                  >
-                    {valor.label}
-                    <i
+            {task.length === 0 ? (
+              <li className="list-group-item">No task, add task</li>
+            ) : (
+              task.map((valor, index) => (
+                <li
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                  key={index}
+                >
+                  {valor}
+				  <i
                       className="fas fa-trash delete-icon"
                       onClick={() => deleteTask(index)}
                     ></i>
-                  </li>
-                );
-              })}
+
+                </li>
+              ))
+            )}
           </ul>
         </div>
         <div className="card-footer text-muted">
-          Things to do {task.length}
+          Things to do: {task.length}
         </div>
       </div>
     </div>
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
+//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
